c782d6cc4520fce6bbf8daf35cca831d4bf3af59
Haiyan Xiao <xiao.hai@husky.neu.edu>
2020-01-27 21:31:44 -0800

    Updated user-admin* in which create and delete is working now.

--- src/main/webapp/user-admin/user-admin.controller.client.js
+++ src/main/webapp/user-admin/user-admin.controller.client.js
@@ -6,32 +6,25 @@
     let firstNameFld = $("#firstNameFld");
     let lastNameFld = $("#lastNameFld");
     let roleFld = $("#roleFld");
-    let userTableFld = $("#userTableFld");
+    let $userTableFld = $("#userTableFld");
     let createBtn = $("#createBtn");
     let removeBtn = $("#removeBtn");
     let editBtn = $("#editBtn");
 
     let users = [];
-    let user1={username : "admin", password : "admin123",firstName : "Admin", lastName : "NEU", role : "ADMIN"};
-    let user2={username : "hxiao", password : "hello123",firstName : "Haiyan", lastName : "Xiao", role : "FACULTY"};
-    let user3={username : "alanhu", password : "alan123",firstName : "Alan", lastName : "Hu", role : "STUDENT"};
+    let user1={username : "admin", password : "admin123",firstName : "Admin", lastName : "NEU", role : "ADMIN", _id : "1"};
+    let user2={username : "hxiao", password : "hello123",firstName : "Haiyan", lastName : "Xiao", role : "FACULTY", _id : "2"};
+    let user3={username : "alanhu", password : "alan123",firstName : "Alan", lastName : "Hu", role : "STUDENT", _id : "3"};
     users.push(user1)
     users.push(user2)
     users.push(user3)
 
-    function deleteUser(index) {
-        let user = users[index];
-        let userId = user._id;
-
-        userService.deleteUser(userId)
-            .then(response => {users.splice(index, 1);
-            renderUsers()
-            })
-    }
 
     function renderUsers() {
-        var table = "";
+        // $userTableFld.children().remove();
+//        $("#userTableBodyFld").children().remove();
         for (var i = 0; i < users.length; i++) {
+            let user = users[i];
             var row = "<tr>";
             var col = "<td>" + users[i].username + "</td>";
             row += col;
@@ -48,22 +41,24 @@
             col = "<td>" + users[i].role + "</td>";
             row += col;
 
-            col = "<td class=\"wbdv-actions\">"
-            col += "<span class=\"float-right\">"
-            col += "<button id=\"removeBtn\">";
-            col += "<i id=\"wbdv-remove\" class=\"fa-2x fa fa-times wbdv-remove\"></i>"
-            col += "</button>";
-            col += "<button id=\"editBtn\">";
-            col += "<i id=\"wbdv-edit\" class=\"fa-2x fa fa-pencil-alt wbdv-edit\"></i>"
-            col += "</button>";
-            col += "</span></td>"
-            row += col;
+            $userTableFld.append(row);
 
-            row += "</tr>";
-            table += row;
+            $td = $("<td class=\"wbdv-actions\">");
+            $span = $("<span class=\"float-right\">")
+
+            $delBtn = $("<button id=\"removeBtn\">"
+                         + "<i id=\"wbdv-remove\" class=\"fa-2x fa fa-times wbdv-remove\"></i>"
+                         + "</button>");
+            $delBtn.click(() => deleteUser(user))
+            $span.append($delBtn);
+
+            $editBtn = $("<button id=\"editBtn\">"
+                        + "<i id=\"wbdv-edit\" class=\"fa-2x fa fa-pencil-alt wbdv-edit\"></i>"
+                        + "</button>");
+            $span.append($editBtn);
+            $td.append($span);
+            $userTableFld.append($td);
         }
-
-        userTableFld.append(table);
     }
 
     function createUser(){
@@ -73,12 +68,24 @@
             lastName : $("#lastNameFld").val(),
             role : $("#roleFld").val()};
 
-
         userService.createUser(newUser).then (actualUser => {
-            console.log(actualUser);
+            console.log("Created a new user: " + JSON.stringify(actualUser));
             users.push(actualUser);
             renderUsers();
         })
+    }
+
+
+    function deleteUser(user) {
+        console.log("Deleted a user: " + JSON.stringify(user));
+        //let user = users[index];
+        //let userId = user._id;
+        let id = user._id;
+
+        userService.deleteUser(id)
+            .then(response => {users.splice(id, 1);
+                renderUsers()
+            })
     }
 
     //removeBtn.mouseup(deleteUser);
