387838ea580e02e94fe0955ff6a1285d7d485b4c
Haiyan Xiao <xiao.hai@husky.neu.edu>
2020-02-15 14:37:23 -0800

    Completed two restful API: findAllWidgets and findWidgetById.

--- src/main/java/com/example/wbdvsp20xiaohaiserverjava/controllers/WidgetController.java
+++ src/main/java/com/example/wbdvsp20xiaohaiserverjava/controllers/WidgetController.java
@@ -1,4 +1,27 @@
 package com.example.wbdvsp20xiaohaiserverjava.controllers;
 
+import com.example.wbdvsp20xiaohaiserverjava.service.WidgetService;
+import com.example.wbdvsp20xiaohaiserverjava.models.Widget;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.ArrayList;
+
+@RestController
+@CrossOrigin(origins = "*")
 public class WidgetController {
+    WidgetService service = new WidgetService();
+
+    @GetMapping("/widgets")
+    public List<Widget> findAllWidgets() {
+        return service.findAllWidgets();
+    }
+
+    @GetMapping("/widget/{widgetId}")
+    public Widget findWidgetById(@PathVariable("widgetId") String wid) {
+        return service.findWidgetById(wid);
+    }
 }
